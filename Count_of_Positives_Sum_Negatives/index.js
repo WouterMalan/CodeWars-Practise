235d77b97e980527a731a30ef67a0b06f7c8c13d
Wouter <woutermalan7909@gmail.com>
2022-06-02 13:01:14 +0200

    added alternative solution

--- Count_of_Positives_Sum_Negatives/index.js
+++ Count_of_Positives_Sum_Negatives/index.js
@@ -26,3 +26,8 @@ function countPositivesSumNegatives(input) {
 }
 
 console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+
+// antoher solution
+// function countPositivesSumNegatives(input) {
+//     return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+// }
